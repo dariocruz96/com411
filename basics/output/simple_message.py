f738f2be1e5087a3ca09dbf08e9945d3e8ae5f44
dariocruz96 <dariocruz96@hotmail.com>
2021-09-30 11:37:11 +0100

    I am Beep! Message

--- basics/output/simple_message.py
+++ basics/output/simple_message.py
@@ -1,3 +1,5 @@
 print("System Failure Imminent!")
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+
+print("I am Beep!")
---
+++ main.py
@@ -0,0 +1,6 @@
+print("This is my first file")
+
+firstArray=[1, 2, 3]
+
+print(firstArray[0], firstArray[1])
+print(firstArray[0]+firstArray[2])
