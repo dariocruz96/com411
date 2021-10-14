aa07d90a0c2f74b8643e0d8bde9b443b590e154d
5rousd70 <dariocruz96@hotmail.com>
2021-10-14 12:45:31 +0100

    Added code to demonstrate the use of return values.

---
+++ basics/functions/return_values.py
@@ -0,0 +1,19 @@
+def sum_weights(weight1, weight2):
+    return weight1 + weight2
+
+
+def calc_avg_weight(weight1, weight2):
+    return sum_weights(weight1, weight2) / 2
+
+
+def run():
+    weight1 = int(input("Please enter the weight of Beep:\n"))
+    weight2 = int(input("Please enter the weight of Bop:\n"))
+    word = input("Sum or average?\n")
+    if word == "sum":
+        print(f"The sum of Beep and Bop's weight is {sum_weights(weight1, weight2)}")
+    elif word == "average":
+        print(f"The average of Beep and Bop's weight is {calc_avg_weight(weight1, weight2)}")
+
+
+run()
