20e58918fe5b69c9ed791007607dd8e94e7e0d3c
dariocruz96 <dariocruz96@hotmail.com>
2021-10-22 05:01:18 +0100

    Added call the function when the module is executed directly

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,9 +1,15 @@
-# Display message to the standard output
-print("System Failure Imminent!")
-print("System reboot has been initiated...")
-print()
-print("...rebooting sensory system")
-print("...rebooting output motors")
-print("...rebooting hover engine")
-print()
-print("System online.")
+def run():
+
+    # Display message to the standard output
+    print("System Failure Imminent!")
+    print("System reboot has been initiated...")
+    print()
+    print("...rebooting sensory system")
+    print("...rebooting output motors")
+    print("...rebooting hover engine")
+    print()
+    print("System online.")
+
+if __name__ == "__main__":
+    run()
+
