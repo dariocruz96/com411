e14cd385a1d64992d3a588a9e2b508e2584e09de
dariocruz96 <dariocruz96@hotmail.com>
2021-10-29 07:23:55 +0100

    Added code to display a menu of options.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -15,9 +15,27 @@ def load_data(file_path):
     print("Done!")
 
 
+def display_menu():
+    print("Please select one of the following options:\n"
+          "[1] Display the names of all passengers\n"
+          "[2] Display the number of passengers that survived\n"
+          "[3] Display the number of passengers per gender\n"
+          "[4] Display the number of passengers per age group\n")
+    while True:
+        choice = int(input())
+        if 4 < choice:
+            print("Sorry, that option is not valid.")
+            continue
+        else:
+            break
+    return choice
+
+
 def run():
     load_data("titanic.csv")
-    print(f"Successfully loaded {len(records)} records.")
+    print(f"Successfully loaded {len(records)} records.\n")
+    selected_option = display_menu()
+    print(f"You have selection option: {selected_option}")
 
 
 if __name__ == "__main__":
