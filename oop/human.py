669637912155fdc4526acda29e3c30e7e1abfbe3
dariocruz96 <dariocruz96@hotmail.com>
2022-01-02 21:38:41 +0000

    Added code to create basic human and robot classes.

---
+++ oop/human.py
@@ -0,0 +1,22 @@
+class Human:
+    MAX_ENERGY = 100
+
+    def __init__(self, name="Human", age=0):
+        self.name = name
+        self.age = age
+        self.energy = Human.MAX_ENERGY
+
+    def __repr__(self):
+        return f'human(name={self.name}, age={self.age}, energy={self.energy}'
+
+    def __str__(self):
+        return f'My name is {self.name} and I am {self.age} years old and my energy is {self.energy}.'
+
+    def display(self):
+        print(f"I am {self.name}")
+
+
+
+if (__name__ == "__main__"):
+    human = Human()
+    human.display()
---
+++ oop/robot.py
@@ -0,0 +1,21 @@
+class Robot:
+    MAX_ENERGY = 100
+
+    def __init__(self, name="Robot", age=0):
+        self.name = name
+        self.age = age
+        self.energy = Robot.MAX_ENERGY
+
+    def __repr__(self):
+        return f'robot(name={self.name}, age={self.age}, energy={self.energy}'
+
+    def __str__(self):
+        return f'My name is {self.name} and I am {self.age} years old and my energy is {self.energy}.'
+
+    def display(self):
+        print(f"I am {self.name}")
+
+
+if (__name__ == "__main__"):
+    robot = Robot()
+    robot.display()
